944db8df81430c0923391fbe43bc9000822f6bf4
Takuya Yamamoto <59943729+oozzZZZZ@users.noreply.github.com>
2021-02-08 14:45:47 +0900

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 14:07:45 2021
+
+@author: t.yamamoto
+"""
+import os
+from tqdm import tqdm
+import random
+import numpy as np
+
+from librosa.util import find_files
+from librosa.core import load,stft,resample
+
+import parameter as C
+
+PATH_FFT = C.PATH_FFT
+SPEECH_PATH = C.SPEECH_PATH
+NOISE_PATH = C.NOISE_PATH
+
+times = C.datatimes
+
+for time in tqdm(range(times)):
+
+    speechlist = find_files(SPEECH_PATH, ext="wav")
+    noiselist = find_files(NOISE_PATH, ext="wav")
+
+    random.shuffle(speechlist)
+    random.shuffle(noiselist)
+
+    if not os.path.exists(PATH_FFT):
+        os.mkdir(PATH_FFT)
+    noise_num = len(noiselist)
+
+    for i in tqdm(range(len(speechlist)-1),leave=False):
+        
+        target, sr = load(speechlist[i], sr=None)
+        if sr != C.SR:
+            target = resample(target, sr, C.SR)
+            
+        #speech data
+        spec = stft(target, n_fft=C.FFT_SIZE, hop_length=C.H, win_length=C.FFT_SIZE)
+        fulllen=spec.shape[1]
+
+        while fulllen<C.PATCH_LENGTH * (C.BATCH_SIZE+1) :
+            i+=1
+            target, sr = load(speechlist[i], sr=None)
+            if sr != C.SR:
+                target = resample(target, sr, C.SR)
+            conc = stft(target, n_fft=C.FFT_SIZE, hop_length=C.H, win_length=C.FFT_SIZE)
+            spec = np.concatenate((spec,conc),1)
+            fulllen = spec.shape[1]
+        speech_spec = spec[:C.PATCH_LENGTH * (C.BATCH_SIZE+1) ]
+
+        #noise data
+        noise, sr = load(noiselist[[random.randint(0, noise_num-1)]], sr=None)
+        if sr != C.SR:
+            noise = resample(noise, sr, C.SR)
+        spec = stft(noise, n_fft=C.FFT_SIZE, hop_length=C.H, win_length=C.FFT_SIZE)
+        fulllen=spec.shape[1]
+
+        while fulllen<C.PATCH_LENGTH * (C.BATCH_SIZE+1) :
+            i+=1
+            noise, sr = load(noiselist[[random.randint(0, noise_num-1)]], sr=None)
+            if sr != C.SR:
+                noise = resample(noise, sr, C.SR)
+            conc = stft(noise, n_fft=C.FFT_SIZE, hop_length=C.H, win_length=C.FFT_SIZE)
+            spec = np.concatenate((spec,conc),1)
+            space = np.zeros([spec.shape[0],random.randint(1,120)])
+            spec = np.concatenate((spec,space),1)
+            fulllen = spec.shape[1]
+        noise_spec = spec[:C.PATCH_LENGTH * (C.BATCH_SIZE+1)]
+
+        #data mixer
+        speech_spec=speech_spec[:,: C.PATCH_LENGTH * C.BATCH_SIZE]
+        noise_spec=noise_spec[:,: C.PATCH_LENGTH * C.BATCH_SIZE]
+        mix_spec=speech_spec+noise_spec
+
+        speech_mag = np.abs(speech_spec)
+        speech_mag /= np.max(speech_mag)
+        mix_mag = np.abs(mix_spec)
+        mix_mag /= np.max(mix_mag)
+
+        for iterate in range(C.BATCH_SIZE):
+            target = speech_mag[:,C.PATCH_LENGTH * iterate:C.PATCH_LENGTH * (iterate+1)]
+            data = mix_mag[:,C.PATCH_LENGTH * iterate:C.PATCH_LENGTH * (iterate+1)]
+            fname = str(i) + "_" + str(iterate) +"_" + str(time)
+            np.savez(os.path.join(PATH_FFT, fname+".npz"),speech=target, addnoise=data)
