4bdac8815115b39487270d7d1fc228c23bf1c387
Takuya Yamamoto <59943729+oozzZZZZ@users.noreply.github.com>
2021-02-15 16:11:47 +0900

    Update parameter.py

--- parameter.py
+++ parameter.py
@@ -8,8 +8,13 @@ Created on Wed Jan 27 17:36:13 2021
 MODEL_PATH = "D:/yamamoto/model_musdb" # save model
 PATH_FFT = "D:/yamamoto/musdb_stft_dataset" # save dataset
 
+#If you have MUDSB18
 MUSDB_PATH = "D:/yamamoto/音源分離用データ/MUSDB" # musdb18 dir path
 target = "vocals" # vocals/drums/bass/other separation target
+
+#If you use your dataset...
+TARGET_PATH = "./speech" #wav
+NOISE_PATH = "./other" #wav
 
 WINDOWS = True
 # Select your OS: Windows=>True / mac or linux=>False
