350744184e8ce571c758df04d6fead8078185c06
Takuya Yamamoto <59943729+oozzZZZZ@users.noreply.github.com>
2021-02-15 15:15:44 +0900

    Update parameter.py

--- parameter.py
+++ parameter.py
@@ -26,7 +26,7 @@ learning_rate=0.0001
 epochs=600
 
 #datasplit
-train,val = 0.8, 0.2
+train,val = 0.8, 0.2 # train + val = 1.0
 
 pre_trained = False
 pre_trained_model = "./model/model_20210208_060155.pt" #事前学習モデル
